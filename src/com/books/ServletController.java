9063a149ff0cba705cce29f958d242a20efe9800
soufiane000 <site.web97@gmail.com>
2021-04-06 15:10:07 +0100

    3commit

--- src/com/books/ServletController.java
+++ src/com/books/ServletController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletController
  */
-@WebServlet("/ServletController")
+//@WebServlet("/Books/*")
 public class ServletController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,13 +26,14 @@ public class ServletController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 PrintWriter out = response.getWriter();
-	       out.println("Welcome to our To Do List app!");
+		 String name= request.getParameter("name");
+	       out.println("Welcome "+name);
 		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		 doGet(request, response);
 	}
 
 }
